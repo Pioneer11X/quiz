8c16ecb4a5943abed85d5c93f25422751c3db7cc
Pioneer11X <sravan@mysmartprice.com>
2016-02-11 15:22:15 +0530

    Created a new Project and added all the basic Stuff / Constraints

---
+++ xcode/Dota 2 Item Quiz/Dota 2 Item Quiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dota 2 Item Quiz
+//
+//  Created by Sravan on 11/02/16.
+//  Copyright © 2016 pioneer11x. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
